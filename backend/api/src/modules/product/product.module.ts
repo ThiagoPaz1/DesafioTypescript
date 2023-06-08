750a5f4cdfcd1d23b9a0c6a677c0439fbaba946d
Thiago da Paz <thiagopazjunior@gmail.com>
2023-06-08 13:02:26 -0300

    feat: VerifyProductIdMiddleware created.

--- backend/api/src/modules/product/product.module.ts
+++ backend/api/src/modules/product/product.module.ts
@@ -8,7 +8,10 @@ import {
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { ProductRepository } from './product.repository';
-import { ValidateProductCreationDataMiddleware } from './middlewares/validateProductCreationData.middlewares';
+import {
+  ValidateProductCreationDataMiddleware,
+  VerifyProductIdMiddleware,
+} from './middlewares';
 
 @Module({
   controllers: [ProductController],
@@ -19,5 +22,13 @@ export class ProductModule implements NestModule {
     consumer
       .apply(ValidateProductCreationDataMiddleware)
       .forRoutes({ path: '/api/v1/products', method: RequestMethod.POST });
+
+    consumer
+      .apply(VerifyProductIdMiddleware)
+      .exclude(
+        { path: '/api/v1/products', method: RequestMethod.GET },
+        { path: '/api/v1/products', method: RequestMethod.POST },
+      )
+      .forRoutes(ProductController);
   }
 }
