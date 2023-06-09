2a09b67503cdff0cf142e76b498675f6c46ed651
Thiago da Paz <thiagopazjunior@gmail.com>
2023-06-09 20:04:09 -0300

    feat: Product component created.

--- challenge/frontend/src/app/components/Products/ProductList/Product.tsx
+++ challenge/frontend/src/app/components/Products/ProductList/Product.tsx
@@ -1,10 +1,29 @@
+import Image from 'next/image'
+
 // Types
 import { ProductProps } from '../types'
+
+// Utils
+import { priceFormat } from '@/app/utils/priceFormat'
 
 export function Product({ ...props }: ProductProps) {
   return (
     <li>
-      {props.name}
+      <Image
+        width={200}
+        height={200}
+        src={props.image}
+        alt="Product image"
+      />
+      <h1>
+        {props.name}
+      </h1>
+      <h3>
+        {props.brand}
+      </h3>
+      <p>
+        {priceFormat(props.price)}
+      </p>
     </li>
   )
 }
