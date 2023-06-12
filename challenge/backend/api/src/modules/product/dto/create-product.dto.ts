166d7dba532a03ffdb1f4b07b5da618c6ef39d46
Thiago da Paz <thiagopazjunior@gmail.com>
2023-06-12 18:23:17 -0300

    feat: Documentation created with swagger.

--- challenge/backend/api/src/modules/product/dto/create-product.dto.ts
+++ challenge/backend/api/src/modules/product/dto/create-product.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty()
   @IsNotEmpty({ message: 'name property cannot be empty.' })
   @IsString({ message: 'name property has to be of type string.' })
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'brand property cannot be empty.' })
   @IsString({ message: 'brand property has to be of type string.' })
   brand: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'image property cannot be empty.' })
   @IsString({ message: 'image property has to be of type string.' })
   image: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'price property cannot be empty.' })
   @IsNumber()
   price: number;
